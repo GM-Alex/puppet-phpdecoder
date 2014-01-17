99cb027faf84de8dacbdac3070b022d31784bbfa
Alexander Schneider <alexander@jankowfsky.com>
2014-01-17 09:27:47 +0100

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,4 @@
+name 'phpdecoder'
+version '1.0.0'
+author 'Jankowfsky AG - Alexander Schneider'
+description 'Puppet module to install ZEND Guard Loader, ZEND Optimizer or IonCube Loader for PHP.'
---
+++ manifests/install.pp
@@ -0,0 +1,42 @@
+define phpdecoder::install (
+  $php_version = '5.3',
+  $type        = 'zend',
+  $modules_dir = '/etc/apache2/modules/zgl/',
+  $php_ini_dir = '/etc/php5/apache2/conf.d/',
+) {
+    if ! defined(File["${modules_dir}"]) {
+        file { "${modules_dir}":
+            ensure => 'directory',
+            mode   => 750,
+            owner  => 'root',
+        }
+    }
+
+    if $type == 'zend' {
+      if versioncmp($php_version, '5.3') < 0 {
+        $decoder_type = 'ZendOptimizer'
+        $ini_file     = 'zendoptimizer.ini'
+      } else {
+        $decoder_type = 'ZendGuardLoader'
+        $ini_file     = 'zendguardloader.ini'
+      }
+    } elsif $type == 'ioncube' {
+        $decoder_type = 'ioncube'
+        $ini_file     = 'ioncubeloader.ini'
+    }
+
+    $short_php_version = regsubst($php_version, '^(\d+)\.(\d+)', '\1.\2')
+
+    file { "${modules_dir}${decoder_type}-php-${short_php_version}.so":
+        ensure    => present,
+        source    => "puppet:///modules/phpdecoder/${decoder_type}-php-${short_php_version}.so",
+        subscribe => File["${modules_dir}"]
+    }
+
+    file { "${php_ini_dir}${ini_file}":
+        ensure    => present,
+        content   => template("phpdecoder/${ini_file}.erb"),
+        subscribe => File["${modules_dir}${decoder_type}-php-${short_php_version}.so"],
+        notify    => Service['httpd']
+    }
+}
