d2b0d592dfb9630158f3b5e6efd213ef2a046b0c
Alexander Schneider <alexander@jankowfsky.com>
2014-01-22 09:48:50 +0100

    Correct regex Add additional checks

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,39 +4,43 @@ define phpdecoder::install (
   $modules_dir = '/etc/apache2/modules/zgl/',
   $php_ini_dir = '/etc/php5/apache2/conf.d/',
 ) {
-    if ! defined(File["${modules_dir}"]) {
-        file { "${modules_dir}":
-            ensure => 'directory',
-            mode   => 750,
-            owner  => 'root',
-        }
-    }
-
-    if $type == 'zend' {
-      if versioncmp($php_version, '5.3') < 0 {
-        $decoder_type = 'ZendOptimizer'
-        $ini_file     = 'zendoptimizer.ini'
-      } else {
-        $decoder_type = 'ZendGuardLoader'
-        $ini_file     = 'zendguardloader.ini'
+  if ! defined(File["${modules_dir}"]) {
+      file { "${modules_dir}":
+          ensure => 'directory',
+          mode   => 750,
+          owner  => 'root',
       }
-    } elsif $type == 'ioncube' {
-        $decoder_type = 'ioncube'
-        $ini_file     = 'ioncubeloader.ini'
+  }
+
+  if $type == 'zend' {
+    if versioncmp($php_version, '5.3') < 0 {
+      $decoder_type = 'ZendOptimizer'
+      $ini_file     = 'zendoptimizer.ini'
+    } else {
+      $decoder_type = 'ZendGuardLoader'
+      $ini_file     = 'zendguardloader.ini'
     }
+  } elsif $type == 'ioncube' {
+      $decoder_type = 'ioncube'
+      $ini_file     = 'ioncubeloader.ini'
+  }
 
-    $short_php_version = regsubst($php_version, '^(\d+)\.(\d+)', '\1.\2')
+  $short_php_version = regsubst($php_version, '^(\d+\.\d+).*$', '\1')
 
+  if ! defined(File["${modules_dir}${decoder_type}-php-${short_php_version}.so"]) {
     file { "${modules_dir}${decoder_type}-php-${short_php_version}.so":
-        ensure    => present,
-        source    => "puppet:///modules/phpdecoder/${decoder_type}-php-${short_php_version}.so",
-        subscribe => File["${modules_dir}"]
+      ensure    => present,
+      source    => "puppet:///modules/phpdecoder/${decoder_type}-php-${short_php_version}.so",
+      subscribe => File["${modules_dir}"]
     }
+  }
 
+  if ! defined(File["${php_ini_dir}${ini_file}"]) {
     file { "${php_ini_dir}${ini_file}":
-        ensure    => present,
-        content   => template("phpdecoder/${ini_file}.erb"),
-        subscribe => File["${modules_dir}${decoder_type}-php-${short_php_version}.so"],
-        notify    => Service['httpd']
+      ensure    => present,
+      content   => template("phpdecoder/${ini_file}.erb"),
+      subscribe => File["${modules_dir}${decoder_type}-php-${short_php_version}.so"],
+      notify    => Service['httpd']
     }
+  }
 }
